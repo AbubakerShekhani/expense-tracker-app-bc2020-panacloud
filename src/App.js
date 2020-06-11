294c5dafe4894501a89d201617a80f65abf2d841
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-12 00:14:50 +0500

    Add Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import AppHeader from './components/Header/Header'
 import Balance from './components/Balance/Balance'
 import IncomeExpense from './components/IncomeExpense/IncomeExpense'
-import TransactionSummary from './components/Transactions/TransactionSummary'
+import TransactionList from './components/Transactions/TransactionList'
 import AddTransaction from './components/Transactions/AddTransaction'
+
+import { GlobalProvider } from './context/GlobalState'
 
 function App() {
   return (
-    <div className="App container">
-      <AppHeader />
-      <Balance />
-      <IncomeExpense />
-      <TransactionSummary />
-      <AddTransaction />
-    </div>
+    <GlobalProvider>
+      <div className="App container">
+        <AppHeader />
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 

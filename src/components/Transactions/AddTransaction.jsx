22a6526c29c3b1d1079dd04ffcc66a109a6ad76e
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-11 20:09:48 +0500

    Add new transaction input form

--- src/components/Transactions/AddTransaction.jsx
+++ src/components/Transactions/AddTransaction.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const AddTransaction = () => {
+  const [note, setNote] = useState('');
+  const [amount, setAmount] = useState(0)
+
   return (<div>
-    <h2>Add Transaction</h2>
-    <p>Note</p>
-    <p>Amount</p>
+    <h2>New Transaction</h2>
+    <form>
+        <div className="form-control">
+          <label htmlFor="text">Note</label>
+          <input type="text" value={note} onChange={(e) => setNote(e.target.value)} placeholder="Enter note for transaction" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
   </div>)
 }
 

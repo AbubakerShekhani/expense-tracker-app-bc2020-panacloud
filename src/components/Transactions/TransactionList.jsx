7fff8345f8bd3db976f8e377cfe2a934ee9f21ac
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-12 16:22:39 +0500

    Update TransactionList.jsx

--- src/components/Transactions/TransactionList.jsx
+++ src/components/Transactions/TransactionList.jsx
@@ -6,21 +6,10 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
-  },
-}));
 
 const TransactionList = () => {
   const { transactions } = useContext(GlobalContext)
-  const classes = useStyles();
+
 
   return (<div>
     <ExpansionPanel defaultExpanded>
